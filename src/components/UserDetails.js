c78dd772a13ab32a54c91d45d5af3260fcd99271
jayesh giramkar <jayeshgiramkars911@gmail.com>
2023-03-05 04:09:13 +0530

    URL Params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { useParams } from "react-router";
 
 export const UserDetails = () => {
-  return <div>Details about the User</div>;
+  const params = useParams();
+  const userId = params.userId;
+
+  //   We can also use destructuring
+  // const { userId } = useParams();
+  return <div>Details about the User {userId}</div>;
 };
